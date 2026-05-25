47ca324edfc24b6718a44536319b31b8e2e89661
unknown <srengseavhou890@gmail.com>
2026-05-25 18:55:18 +0700

    new build.sh

--- build.sh
+++ build.sh
@@ -4,3 +4,9 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --noinput
 python manage.py migrate
+python manage.py shell -c "
+from django.contrib.auth import get_user_model
+User = get_user_model()
+if not User.objects.filter(username='$ADMIN_USERNAME').exists():
+    User.objects.create_superuser('$ADMIN_USERNAME', '$ADMIN_EMAIL', '$ADMIN_PASSWORD')
+" 
